b6d07dc0d10dcb7fc93fb1535e0c4e28d5349a0a
Lawrence <lawrenxew@gmail.com>
2024-01-16 18:18:18 +1100

    change bubble size

--- src/Components/Bubble.tsx
+++ src/Components/Bubble.tsx
@@ -10,7 +10,7 @@ interface BProps {
 const Bubble = ({ children, className, link, onClick }: BProps) => {
   return (
     <a
-      className={`  border-black border-2 w-fit text-sm lg:text-md xl:text-lg px-2 py-1 font-medium ${className}`}
+      className={`  border-black border-2 w-fit text-sm lg:text-md xl:text-md px-2 py-1 font-medium ${className}`}
       href={link}
       target="_blank"
       onClick={onClick}
